21680b4321cd430371f67157fa0e1bedbeacafdb
root <root@rpi-web.(none)>
2014-10-05 23:48:08 -0500

    Add JSON format option.

--- cron-run.sh
+++ cron-run.sh
@@ -2,24 +2,37 @@
 #Derived from script @ http://www.linuxjournal.com/content/temper-pi
 #script provided "as-is" with no guarantees.
 
+FORMAT="JSON" #Formats: RAW | JSON
 TEMP_MIN="32"
 TEMP_MAX="99"
 
-LOGFILE='/var/log/temper.log'
+LOGFILE='/var/log/temper.json'
 TIME=`date +"%b %d %T"`
-TEMPERATURE=`temper-poll 2>/dev/null | tail -n1 | cut -f4 -d ' ' | sed 's/.F$//'`
+TEMPERATURE=`/usr/local/bin/temper-poll 2>/dev/null | tail -n1 | cut -f4 -d ' ' | sed 's/.F$//'`
 
+STATUS='ERROR'
 if [[ $TEMPERATURE == "" ]]; then
-  echo ERROR
+
+	if [[ $FORMAT ==  "RAW" ]]; then
+		echo  -e "$TIME\t$TEMPERATURE\t$STATUS" >> $LOGFILE
+	elif [[ $FORMAT == "JSON" ]]; then
+		echo -e "{'timestamp':'$TIME','value':'$TEMPERATURE','status':'$STATUS'}" >> $LOGFILE
+	fi
   exit 1
 fi
 
+
+STATUS='NORMAL'
 if [[ $TEMPERATURE < $TEMP_MIN ]]; then  #too cold
-  echo -e "$TIME\t$TEMPERATURE\tCOLD" >> $LOGFILE
-  exit 0
+	STATUS='COLD'
 elif [[ $TEMPERATURE > $TEMP_MAX ]]; then #too hot
-  echo -e "$TIME\t$TEMPERATURE\tHOT" >> $LOGFILE
-  exit 0
+	STATUS='HOT'
 fi
-echo -e "$TIME\t$TEMPERATURE\tNormal" >> $LOGFILE
+
+if [[ $FORMAT == 'RAW' ]]; then
+	echo -e "$TIME\t$TEMPERATURE\t$STATUS" >> $LOGFILE
+elif [[ $FORMAT == 'JSON' ]]; then
+	echo -e "{'timestamp':'$TIME','value':'$TEMPERATURE','status':'$STATUS'}" >> $LOGFILE
+fi
+
 exit 0
